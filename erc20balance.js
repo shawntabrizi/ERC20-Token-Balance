c19c54fdaf2000128d6856b8548eaab554d68e59
Eric Youngson <eayoungs@gmail.com>
2018-06-26 18:02:24 -0700

    Added dropdown menu to choose specific address for history lookup, reformatting

--- erc20balance.js
+++ erc20balance.js
@@ -1,56 +1,56 @@
 window.addEventListener('load', function () {
-    if (typeof web3 !== 'undefined') {
-        console.log('Web3 Detected! ' + web3.currentProvider.constructor.name)
-        window.web3 = new Web3(web3.currentProvider);
-    } else {
-        console.log('No Web3 Detected... using HTTP Provider')
-        window.web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/<APIKEY>"));
-    }
+  if (typeof web3 !== 'undefined') {
+    console.log('Web3 Detected! ' + web3.currentProvider.constructor.name)
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.log('No Web3 Detected... using HTTP Provider')
+    window.web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/<APIKEY>"));
+  }
 })
 
 const promisify = (inner) =>
-    new Promise((resolve, reject) =>
-        inner((err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        })
-    );
+  new Promise((resolve, reject) =>
+    inner((err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    })
+  );
 
 async function getBalance() {
-    var address, wei, balance
-    address = document.getElementById("address").value;
-    wei = promisify(cb => web3.eth.getBalance(address, cb))
-    try {
-        balance = web3.fromWei(await wei, 'ether')
-        document.getElementById("output").innerHTML = balance + " ETH";
-    } catch (error) {
-        document.getElementById("output").innerHTML = error;
-    }
+  var address, wei, balance
+  address = document.getElementById("address").value;
+  wei = promisify(cb => web3.eth.getBalance(address, cb))
+  try {
+    balance = web3.fromWei(await wei, 'ether')
+    document.getElementById("output").innerHTML = balance + " ETH";
+  } catch (error) {
+    document.getElementById("output").innerHTML = error;
+  }
 }
 
 async function getERC20Balance() {
-    var address, contractAddress, contractABI, tokenContract, decimals, balance, name, symbol, adjustedBalance
-    address = document.getElementById("address").value
-    contractAddress = document.getElementById("contractAddress").value
-    contractABI = human_standard_token_abi
+  var address, contractAddress, contractABI, tokenContract, decimals, balance, name, symbol, adjustedBalance
+  address = document.getElementById("address").value
+  contractAddress = document.getElementById("contractAddress").value
+  contractABI = human_standard_token_abi
 
-    tokenContract = web3.eth.contract(contractABI).at(contractAddress)
+  tokenContract = web3.eth.contract(contractABI).at(contractAddress)
 
-    decimals = promisify(cb => tokenContract.decimals(cb))
-    balance = promisify(cb => tokenContract.balanceOf(address, cb))
-    name = promisify(cb => tokenContract.name(cb))
-    symbol = promisify(cb => tokenContract.symbol(cb))
+  decimals = promisify(cb => tokenContract.decimals(cb))
+  balance = promisify(cb => tokenContract.balanceOf(address, cb))
+  name = promisify(cb => tokenContract.name(cb))
+  symbol = promisify(cb => tokenContract.symbol(cb))
 
-    try {
-        adjustedBalance = await balance / Math.pow(10, await decimals)
-        document.getElementById("output2").innerHTML = adjustedBalance;
-        document.getElementById("output2").innerHTML += " " + await symbol + " (" + await name + ")";
-    } catch (error) {
-        document.getElementById("output2").innerHTML = error;
-    }
+  try {
+    adjustedBalance = await balance / Math.pow(10, await decimals)
+    document.getElementById("output2").innerHTML = adjustedBalance;
+    document.getElementById("output2").innerHTML += " " + await symbol + " (" + await name + ")";
+  } catch (error) {
+    document.getElementById("output2").innerHTML = error;
+  }
 }
 
 function appendToLocalStorage(datum, localStorageName) {
@@ -76,15 +76,15 @@ function appendToLocalStorage(datum, localStorageName) {
 
 // Get the first transaction block for an address
 async function getFirstBlock(address) {
-    let response = await fetch("https://api.etherscan.io/api?module=account&action=txlist&address=" + address + "&startblock=0&page=1&offset=10&sort=asc");
-    let data = await response.json();
+  let response = await fetch("https://api.etherscan.io/api?module=account&action=txlist&address=" + address + "&startblock=0&page=1&offset=10&sort=asc");
+  let data = await response.json();
  
-    return data.result[0].blockNumber;
+  return data.result[0].blockNumber;
 }
 
 async function getBalanceInRange(address, firstBlock) {
-    let wei = web3.eth.getBalance(address, firstBlock);
-    let ether = parseFloat(web3.fromWei(wei, 'ether'));
+  let wei = web3.eth.getBalance(address, firstBlock);
+  let ether = parseFloat(web3.fromWei(wei, 'ether'));
 
-    return ether;
+  return ether;
 }
